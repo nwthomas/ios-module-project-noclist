1ff3067f654fe7964cda6c1f03b65679cfec103c
Nathan Thomas <nwthomas@me.com>
2019-11-03 18:14:03 -0800

    Add function to find clean agents and print them out

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -63,14 +63,20 @@ let compromised = compromisedAgents(agents: agents)
 print("\(compromised) agents have been compromised!")
 //: ## Step 5
 //: Create a function called "findCleanAgents" that both prints the cover names of all uncompromised agents, as well as returns an array of agents that are uncompromised.
-
-
-
+func findCleanAgents(agents: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)]) -> [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] {
+    var total: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] = []
+    for agent in agents {
+        if !agent.compromised {
+            total.append(agent)
+            print(agent.coverName)
+        }
+    }
+    return total
+}
 //: ## Step 6
 //: Call the above function to find the total number of clean agents and print a message that says "# clean agents out of # total agents." Use the total number of agents in the array from step 2 as the second number in the string.
-
-
-
+let cleanAgents = findCleanAgents(agents: agents)
+print("\(cleanAgents.count) clean agents out of \(agents.count) total agents")
 //: ## Step 7
 //: Create a function called "findHighRisk" that prints out the real names and access levels of agents with level 8 or higher. If one of these agents is also currently compromised, add `**WARNING** **COMPROMISED**` to the end of the string that includes their name and access level.
 //: - Example: `Jon Voight, level: 9 **WARNING** **COMPROMISED**`
